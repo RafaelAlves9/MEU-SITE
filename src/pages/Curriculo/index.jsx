d02d11f18e4a951c2169c3ea3d8226ac800c33df
Rafael Alves <hrafael.alves9@gmail.com>
2022-04-27 12:01:25 -0300

    up curriculo

--- src/pages/Curriculo/index.jsx
+++ src/pages/Curriculo/index.jsx
@@ -3,6 +3,9 @@ import { ContentPage } from '../../components/ContentPage';
 import { Container } from '@mui/material';
 import * as S from '../../components/ContentPage';
 import { FooterPage } from "../../components/Footer";
+import { CurriculoContent } from './styled'
+import CV from './file/Rafael-Alves--Dev.pdf';
+import CapaCv from './file/capa-cv.png';
 
 export const Curriculo = () => {
     
@@ -13,6 +16,7 @@ export const Curriculo = () => {
           <Container>
               <S.TituloCategoria>Meu Currículo</S.TituloCategoria>
               <S.SubTituloCategoria>Baixe meu currículo em PDF</S.SubTituloCategoria>
+              <CurriculoContent><a href={CV} target='_blank'><img src={CapaCv} alt="" /></a></CurriculoContent>
           </Container>
         </ContentPage>
         <FooterPage/>
---
+++ src/pages/Curriculo/styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CurriculoContent = styled.div`
+    margin-top: 35px;
+
+    img{
+        max-width: 250px;
+        height: 300px;
+    }
+`
